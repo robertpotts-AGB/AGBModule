c776634e34c499c0808e2046b4c0aaac1e1fa27b
Robert Potts <robert@pottsy.me.uk>
2022-05-23 17:54:25 +0100

    Remove SetYHB and Versions - function in system

--- AGB1/ImperialRounds.php
+++ AGB1/ImperialRounds.php
@@ -10,10 +10,6 @@ require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
 require_once('Common/Fun_FormatText.inc.php');
 include('Common/Templates/head.php');
 
-if(array_key_exists('test',$_POST)) {
-    setyhb();
-}
-
 if(array_key_exists('EvCode',$_POST)){
     setBookingInRef();
 }
@@ -21,95 +17,6 @@ if(array_key_exists('OldCode',$_POST)){
     replace_in_file();
 }
 
-if(array_key_exists('DocVersMin',$_POST)){
-SetVersions();
-}
-
-function setyhb()
-{
-    /**  Collect current event, set base event info */
-    $Tour11 = $_SESSION['TourId'];
-    $updQuery = "UPDATE Tournament SET ToNumDist = '3',ToMaxDistScore='648',ToNumEnds='12',ToXNine='Golds',ToXNineChars='J',ToGolds='Hits',ToGoldsChars='BDFHJ', ToCategory='1'WHERE ToID ='$Tour11'";
-    $updateTournament = safe_r_sql($updQuery);
-
-    echo "Setting Base Info";
-    echo "<br>";
-    echo "Setting scorecard terminology";
-    echo "<br>";
-
-    /**  Setup Distance Parameters */
-    $DropDistances = "DELETE FROM DistanceInformation WHERE DiTournament= $Tour11";
-
-    echo "Removing Distances";
-    echo "<br>";
-
-    $updQuery2 = "INSERT INTO DistanceInformation SET DiSession='1', DiEnds='12', DiArrows='6',DiTournament= $Tour11, DiDistance='1'";
-    $updQuery3 = "INSERT INTO DistanceInformation SET DiSession='1', DiEnds='8', DiArrows='6', DiTournament= $Tour11, DiDistance='2'";
-    $updQuery4 = "INSERT INTO DistanceInformation SET DiSession='1', DiEnds='4', DiArrows='6', DiTournament= $Tour11, DiDistance='3'";
-
-    $updateDistances = safe_r_sql($DropDistances);
-    $updateD1 = safe_r_sql($updQuery2);
-    $updateD2 = safe_r_sql($updQuery3);
-    $updateD3 = safe_r_sql($updQuery4);
-
-    echo "Setting number of Arrows per distance";
-    echo "<br>";
-
-    /** Create Class for M+W */
-    $UpdateClassM = "REPLACE INTO Classes SET ClId='M',ClTournament='$Tour11',ClDescription='Gentlemen',ClViewOrder='9',ClAgeFrom='19',ClAgeTo='100',ClValidClass='M',ClSex='0',ClAthlete='1',ClRecClass='M',ClWaClass='M',ClTourRules='UK|Type_Indoor 18|SetUkJunNationals' ";
-    $UpdateClassW = "REPLACE INTO Classes SET ClId='W',ClTournament='$Tour11',ClDescription='Ladies',ClViewOrder='10',ClAgeFrom='19',ClAgeTo='100',ClValidClass='W',ClSex='1',ClAthlete='1',ClRecClass='W',ClWaClass='W',ClTourRules='UK|Type_Indoor 18|SetUkJunNationals' ";
-
-    $updateClass = safe_r_sql($UpdateClassM);
-    $updateClass = safe_r_sql($UpdateClassW);
-
-    echo "Setting additional classes";
-    echo "<br>";
-
-    /**  Setup Distances for each category */
-
-    $removeDist = "DELETE FROM TournamentDistances WHERE TdTournament=$Tour11";
-    $UpdateM = "REPLACE INTO TournamentDistances SET TdClasses='_M', TDType='6',TDTournament= '$Tour11',Td1='100y',Td2='80y',Td3='60y'";
-    $UpdateW = "REPLACE INTO TournamentDistances SET TdClasses='_W', TDType='6',TDTournament= '$Tour11',Td1='80y',Td2='60y',Td3='50y'";
-    $UpdateS1 = "REPLACE INTO TournamentDistances SET TdClasses='_S1', TDType='6',TDTournament= '$Tour11',Td1='80y',Td2='60y',Td3='50y'";
-    $UpdateS2 = "REPLACE INTO TournamentDistances SET TdClasses='_S2', TDType='6',TDTournament= '$Tour11',Td1='60y',Td2='50y',Td3='40y'";
-    $UpdateS3 = "REPLACE INTO TournamentDistances SET TdClasses='_S3', TDType='6',TDTournament= '$Tour11',Td1='60y',Td2='50y',Td3='40y'";
-    $UpdateS4 = "REPLACE INTO TournamentDistances SET TdClasses='_S4', TDType='6',TDTournament= '$Tour11',Td1='50y',Td2='40y',Td3='30y'";
-    $UpdateS5 = "REPLACE INTO TournamentDistances SET TdClasses='_S5', TDType='6',TDTournament= '$Tour11',Td1='50y',Td2='40y',Td3='30y'";
-    $UpdateS6 = "REPLACE INTO TournamentDistances SET TdClasses='_S6', TDType='6',TDTournament= '$Tour11',Td1='40y',Td2='30y',Td3='20y'";
-    $UpdateS7 = "REPLACE INTO TournamentDistances SET TdClasses='_S7', TDType='6',TDTournament= '$Tour11',Td1='40y',Td2='30y',Td3='20y'";
-    $UpdateS8 = "REPLACE INTO TournamentDistances SET TdClasses='_S8', TDType='6',TDTournament= '$Tour11',Td1='30y',Td2='20y',Td3='10y'";
-
-
-    $removeDistance = safe_r_SQL($removeDist);
-    $updateRounds = safe_r_sql($UpdateS1);
-    $updateRounds = safe_r_sql($UpdateS2);
-    $updateRounds = safe_r_sql($UpdateS3);
-    $updateRounds = safe_r_sql($UpdateS4);
-    $updateRounds = safe_r_sql($UpdateS5);
-    $updateRounds = safe_r_sql($UpdateS6);
-    $updateRounds = safe_r_sql($UpdateS7);
-    $updateRounds = safe_r_sql($UpdateS8);
-    $updateRounds = safe_r_sql($UpdateM);
-    $updateRounds = safe_r_sql($UpdateW);
-
-    echo "Setting Tournament Distance defaults";
-    echo "<br>";
-
-    /** Setup Target Face */
-    $TfRemove = "DELETE FROM TargetFaces WHERE TfTournament ='$Tour11'";
-    $Tfupdate = "REPLACE INTO TargetFaces SET TfId ='1',TfName='All', TfTournament= '$Tour11', TfClasses='%', TfGolds='Hits',TfXNine='Golds', TfGoldsChars='BDFHJ', TfXNineChars='J',TfT1='5',TfW1='122',TfT2='5',TfW2='122',TfT3='5',TfW3='122',TfDefault='1',TfTourRules='UK|Type_Indoor 18|SetUkJunNationals'";
-
-    $updateTargets = safe_r_sql($TfRemove);
-    $updateTargets = safe_r_sql($Tfupdate);
-
-    echo "Replacing Target Face";
-    echo "<br>";
-
-    echo "Done! Please check the event setup to ensure it is correct";
-
-    /** TODO: WRITE AUTO EVENT CREATION BASED ON ARRAY OF BOWSTYLE AND CLASS */
-
-}
 
 function setBookingInRef()
 {
@@ -148,27 +55,7 @@ echo "could Not Read";   }
     {
 echo "Could not write";   }
 }
-function SetVersions(){
-    $Tour11 = $_SESSION['TourId'];
-    $MajVer = ($_POST['DocVersMaj']);
-    $MinVer = ($_POST['DocVersMin']);
-    $DocNote = ($_POST['Notes']);
-    $CurrTime = date('Y-m-d H:i:s');
-    if(is_null($MajVer)){
-        $UpdateSCHED = "REPLACE INTO DocumentVersions SET DvTournament='$Tour11',DvFile='SCHED',DvMinVersion='$MinVer',DvPrintDateTime='$CurrTime',DvIncludedDateTime='$CurrTime',DvNotes='$DocNote'";
-        $UpdateFOP = "REPLACE INTO DocumentVers
-  ions SET DvTournament='$Tour11', DvFile='FOP',DvMinVersion='$MinVer',DvPrintDateTime='$CurrTime',DvIncludedDateTime='$CurrTime',DvNotes='$DocNote'";
-    }
-    else {
-        $UpdateSCHED = "REPLACE INTO DocumentVersions SET DvTournament='$Tour11',DvFile='SCHED',DvMajVersion='$MajVer',DvMinVersion='$MinVer',DvPrintDateTime='$CurrTime',DvIncludedDateTime='$CurrTime',DvNotes='$DocNote'";
-        $UpdateFOP = "REPLACE INTO DocumentVersions SET DvTournament='$Tour11', DvFile='FOP',DvMajVersion='$MajVer',DvMinVersion='$MinVer',DvPrintDateTime='$CurrTime',DvIncludedDateTime='$CurrTime',DvNotes='$DocNote'";
-    }
 
-    $updateSC = safe_r_sql($UpdateSCHED);
-    $updateFP = safe_r_sql($UpdateFOP);
-
-
-}
     ?>
 <style type="text/css">
 
@@ -187,17 +74,7 @@ function SetVersions(){
 
 
 <table class="outlined">
-    <tr>
-        <td>
-            It is important that you have configured a WA18 under AGB Rules with the 'Junior Championships' subclass before proceeding further
-        </td>
-        <td>
 
-            <form method="post">
-                <input style="border-radius:8px;font-size:16px;background-color: orangered;color: white; padding: 15px 32px;" type="submit" name="test" id="test" value="Set up Y/H/B Event" /><br/>
-            </form>
-        </td>
-    </tr>
     <tr>
         <td >
             <h2>Ranking System Exports</h2>
@@ -324,25 +201,7 @@ function SetVersions(){
         </td>
 
     </tr>
-    <tr>
-        <td>
-            <h2> Schedule Version</h2>
-            <h3> Specify the Schedule and FOP versions, and any notes associated and click 'Set Version'</h3>
-        </td>
-
-        <td align="center">
-            <form method="post" action="<?php $_SERVER['PHP_SELF'] ?>">
-                <p>Major Version: <input style="font-size:16px" type="Text" name="DocVersMaj"></p>
-                <p>Minor Version: <input style="font-size:16px" type="Text" name="DocVersMin"></p>
-               <td><p>Notes: <input style="font-size:16px" type="Text" name="Notes"></p></td>
-        <td> <input style="border-radius:8px;font-size:16px;background-color: Green;color: white; padding: 15px 32px;" type="submit" value="Set Version">
-        </td>
-        </form>
-        </td>
-        </td>
-
-    </tr>
-     <tr>
+       <tr>
         <td>
             <h2> WAP Configuration</h2>
         </td>
